930bce3de1d6c9d5397295c300031b57c5a1ad38
mahong <1025383143@qq.com>
2016-07-22 01:34:06 +0800

    complete the code

---
+++ spec/testSpec.js
@@ -0,0 +1,58 @@
+'use strict';
+const main=require('../main/main.js');
+describe('printBarcode', () => {
+
+    let correctZipCodes;
+    let wrongZipCodes;
+
+    beforeEach(() => {
+       correctZipCodes='95713';
+        wrongZipCodes='957';
+    });
+    
+    it('printBarcode', () => {
+
+        spyOn(console, 'log');
+
+        main.printBarcode(correctZipCodes);
+
+        const expectNumber ='||:|:::|:|:|:::|:::||::||::|:|:|';
+
+        expect(console.log).toHaveBeenCalledWith(expectNumber);
+    });
+
+    it('printBarcode', () => {
+
+        spyOn(console, 'log');
+
+        main.printBarcode(wrongZipCodes);
+
+        const expectNumber ='非法邮编';
+
+        expect(console.log).toHaveBeenCalledWith(expectNumber);
+    });
+
+    
+    it('should get correct zipCodes', () => {
+        
+        const result=main.checkZipCode(correctZipCodes);
+        expect(result).toEqual('95713');
+    });
+
+    it('should get wrong information', () => {
+
+        const result= main.checkZipCode(wrongZipCodes);
+        expect(result).toEqual(false);
+    });
+
+    it('buildCodesCheckBit', () => {
+        const checkCodes= main.buildCodesCheckBit('95713');
+        expect(checkCodes).toEqual(['9', '5', '7', '1', '3', '5' ]);
+    });
+
+    it('buildBarcode',()=>{
+        const expectBarcode='||:|:::|:|:|:::|:::||::||::|:|:|';
+        const barcode=main.buildBarcode(['9', '5', '7', '1', '3', '5' ]);
+        expect(expectBarcode).toEqual(barcode);
+    });
+});
